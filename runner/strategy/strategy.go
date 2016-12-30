022de518314d34bf4947add1a3953edef0c82419
Nikita Koptelov <nikita.koptelov@lazada.com>
2016-12-30 11:32:39 +0300

    Strategies give out chans now for better composition

--- runner/strategy/strategy.go
+++ runner/strategy/strategy.go
@@ -2,37 +2,44 @@ package strategy
 
 import "time"
 
-// RequestEnqueueFunc is a function that inserts a request to the
-// pipeline of requests waiting to be sent.
-type RequestEnqueueFunc func()
+// RequestEnqueuer is a chan that signals when a request should be sent to the
+// worker pool.
+//
+// Chan is given out by the Strategy constructor and is closed after all requests
+// had been processed.
+type RequestEnqueuer <-chan struct{}
 
 // RunnerFunc is a function that launches strategy fanout.
 type RunnerFunc func()
 
 // RampUp is a strategy that sends more requests with each tick,
 // starting from initial r/s to ceil r/s.
-func RampUp(count, initial, ceil uint64, eFunc RequestEnqueueFunc) RunnerFunc {
+func RampUp(count, initial, ceil uint64) (RunnerFunc, RequestEnqueuer) {
+	retChan := make(chan struct{}, ceil)
 	return func() {
+		defer close(retChan)
 
 		var currentSimCount = initial
 		for i := uint64(0); i < count; i++ {
 			for j := uint64(0); j < currentSimCount; j++ {
-				eFunc()
+				retChan <- struct{}{}
 			}
 			if currentSimCount < ceil {
 				currentSimCount++
 			}
 		}
-	}
+	}, retChan
 }
 
 // ConstQPS sends requests at a constant rate per second.
-func ConstQPS(total uint64, qps uint64, eFunc RequestEnqueueFunc) RunnerFunc {
-	var throttle = time.Tick(time.Duration(1e6/qps) * time.Microsecond)
+func ConstQPS(total uint64, qps uint64) (RunnerFunc, RequestEnqueuer) {
+	throttle := time.Tick(time.Duration(1e6/qps) * time.Microsecond)
+	retChan := make(chan struct{}, qps*2)
 	return func() {
+		defer close(retChan)
 		for i := uint64(0); i < total; i++ {
 			<-throttle
-			eFunc()
+			retChan <- struct{}{}
 		}
-	}
+	}, retChan
 }
